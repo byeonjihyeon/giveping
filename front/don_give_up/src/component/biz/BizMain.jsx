6091d1d7010bd76e31b8a948c4190431a9a6e596
hi09067@gmail.com <hi09067@gmail.com>
2025-06-30 19:21:22 +0900

    [김은서] 프런트 : 기부 사업 리스트 카테고리 기능 추가

--- front/don_give_up/src/component/biz/BizMain.jsx
+++ front/don_give_up/src/component/biz/BizMain.jsx
@@ -1,5 +1,6 @@
-import BizList from "./BizList";
 import {Route, Routes} from "react-router-dom";
+import BizList from "./BizList";
+import BizView from "./BizView";
 
 // 기부 사업 게시판 메인
 export default function BizMain(){
@@ -8,6 +9,7 @@ export default function BizMain(){
     return (
             <Routes>
                 <Route path="list" element ={<BizList />} />
+                <Route path="view/:bizNo" element ={<BizView />} />
             </Routes>
     );
 }
