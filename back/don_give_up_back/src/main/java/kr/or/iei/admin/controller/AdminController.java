01f44f3ceb6a32ccd0255de32722f3257e0c989e
DESKTOP-AEAO1QC\user1 <ujin6991@gmail.com>
2025-07-01 14:24:37 +0900

    전체회원조회

--- back/don_give_up_back/src/main/java/kr/or/iei/admin/controller/AdminController.java
+++ back/don_give_up_back/src/main/java/kr/or/iei/admin/controller/AdminController.java
@@ -1,11 +1,18 @@
 package kr.or.iei.admin.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.iei.admin.model.service.AdminService;
+import kr.or.iei.common.model.dto.ResponseDTO;
 
 @RestController
 @CrossOrigin("*")
@@ -14,5 +21,22 @@ public class AdminController {
 	
 	@Autowired
 	private AdminService service;
+	
+	//전체 회원 목록 조회
+	@GetMapping
+	public ResponseEntity<ResponseDTO> selectMemberList(@PathVariable int reqPage){
+		ResponseDTO res= new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR, "회원 목록 조회 중,오류가 발생하였습니다",null, "error");
+		
+		try {
+			HashMap<String, Object> memberMap = service.selectMemberList(reqPage);
+			res= new ResponseDTO(HttpStatus.OK, "", memberMap,"");
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}
+		return new ResponseEntity<ResponseDTO>(res,res.getHttpStatus());
+
+	}
+	
 	
 }
--- back/don_give_up_back/src/main/java/kr/or/iei/admin/model/service/AdminService.java
+++ back/don_give_up_back/src/main/java/kr/or/iei/admin/model/service/AdminService.java
@@ -1,12 +1,39 @@
 package kr.or.iei.admin.model.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.iei.admin.model.dao.AdminDao;
+import kr.or.iei.common.model.dto.PageInfo;
+import kr.or.iei.common.util.PageUtil;
+import kr.or.iei.member.model.dto.Member;
 
 @Service
 public class AdminService {
+	
 	@Autowired 
 	private AdminDao dao;
+	
+	@Autowired
+	private PageUtil pageUtil;
+
+	public HashMap<String, Object> selectMemberList(int reqPage) {
+		
+		int viewCnt = 10;   //한 페이지당 보여줄 게시글 갯수 (기존 게시글 목록과 다르게 10개씩 보여줌)
+		int pageNaviSize = 5;    //페이지 네비게이션 길이
+		int totalCount =dao.selectMemberCount();
+		
+		PageInfo pageInfo = pageUtil.getPageInfo(reqPage, viewCnt, pageNaviSize, totalCount);
+		
+		ArrayList<Member> memberList =dao.selectMemberList(pageInfo);
+		
+		HashMap<String,Object>memberMap =new HashMap<String,Object>();
+	    memberMap.put("memberList", memberList);
+		memberMap.put("pageInfo", pageInfo);
+		
+		return memberMap;
+	}
 }
