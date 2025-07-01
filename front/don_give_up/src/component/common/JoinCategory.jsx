2779c1d63f093594030a4eefeb9a6a00dd548355
DESKTOP-6LHTD8G\user1 <qor659659@gmail.com>
2025-07-01 14:08:34 +0900

    [백인환] 프론트 : 회원가입 기능 완료 (css만 남음)

--- front/don_give_up/src/component/common/JoinCategory.jsx
+++ front/don_give_up/src/component/common/JoinCategory.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import createInstance from "../../axios/Interceptor";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 //회원가입 시 기부 카테고리 선택 페이지
 export default function JoinCategory(props) {
@@ -10,6 +11,8 @@ export default function JoinCategory(props) {
 
     const member = props.member;
     const setMember = props.setMember;
+    const org = props.org;
+    const setOrg = props.setOrg;
 
     //DB에서 조회한 카테고리 정보를 저장할 State 변수
     const [donateCtgList, setDonateCtgList] = useState([]);
@@ -33,20 +36,36 @@ export default function JoinCategory(props) {
         setMember({...member, categoryList : checkCtgList});
     }, [checkCtgList]);
 
-    // 회원가입 버튼 클릭 시
-    function joinMember() {
-        
-        console.log(checkCtgList);
-        console.log(member);
+    useEffect(function(){
+        setOrg({...org, categoryList : checkCtgList});
+    }, [checkCtgList]);
 
+    // 회원가입 버튼 클릭 시
+    function join() {
         let options = {};
-        options.url = serverUrl + "/member/join";
-        options.method = "post";
-        options.data = member;
+        if(member.memberId != ""){
+            options.url = serverUrl + "/member/join";
+            options.method = "post";
+            options.data = member;
+        }else{
+            options.url = serverUrl + "/org/join";
+            options.method = "post";
+            options.data = org;
+        }
 
         axiosInstance(options)
         .then(function(res){
-            console.log(res);
+            Swal.fire({
+                title : "알림",
+                text : res.data.clientMsg,
+                icon : res.data.alertIcon,
+                confirmButtonText : "확인"
+            })
+            .then(function(result){
+                if(res.data.resData){ //회원가입 정상 처리
+                    navigate("/login"); //로그인 컴포넌트로 전환
+                }
+            });
         })
         .catch(function(err){
             console.log(err);
@@ -55,8 +74,12 @@ export default function JoinCategory(props) {
 
     return (
         <section className="section category-wrap">
+            {member.memberId != ""
+            ?
             <div className="page-title">관심 카테고리 선택</div>
+            :
             <div className="page-title">주요 카테고리 선택</div>
+            }
             <p>중복 선택 가능</p>
 
             <ul className="select-ctg-wrap">
@@ -66,7 +89,7 @@ export default function JoinCategory(props) {
             </ul>
 
             <div style={{ marginTop: '20px' }}>
-                <button onClick={joinMember}>회원가입</button>
+                <button onClick={join}>회원가입</button>
             </div>
         </section>
         );
