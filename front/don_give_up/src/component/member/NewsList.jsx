8e0b1d6a782675e8d1f9c14350805f10dc6f3564
hi09067@gmail.com <hi09067@gmail.com>
2025-07-04 16:45:26 +0900

    [김은서] 프런트 : 마이페이지 내소식 추가 구현 (사업명, 기관명 추가)

--- front/don_give_up/src/component/member/NewsList.jsx
+++ front/don_give_up/src/component/member/NewsList.jsx
@@ -26,7 +26,7 @@ export default function NewsList(){
 
     useEffect(function(){
         let options = {};
-        options.url = serverUrl + '/member/news/' + memberNo;
+        options.url = serverUrl + '/member/alarm/' + memberNo;
         options.method = 'get';
 
         axiosInstance(options)
@@ -40,7 +40,7 @@ export default function NewsList(){
 
     return (
         <div className="newsList-wrap" >
-                {newsList.map(function(news,index){
+                {newsList.map(function(news, index){
                      return <News key={"news" + index} news={news} />   
                 })}
         </div>
@@ -51,25 +51,25 @@ export default function NewsList(){
 function News(props){
     const news = props.news;
 
+    let content;
+    if(news.alarmType === 0){
+        content = `[사업종료] ${news.orgName} 의 "${news.bizName}" 사업이 종료되었습니다. 설문조사를 해주세요.`;
+    }else if(news.alarmType === 1){
+        content = `[첨부파일업로드] ${news.orgName} 의 "${news.bizName}" 사업이 업데이트 되었습니다.`;
+    }else if(news.alarmType === 2){
+        content = `[관심단체] ${news.orgName} 의 새로운 소식이 업데이트 되었습니다.`;
+    }
+
     return (
-
-        
-            <div className="news-info">
-                {/* 알림 종류(0 : 사업 종료날(설문조사필수), 1 : 보고서 등 사업 상세 페이지에 첨부파일 업로드 시, 2 : 관심 단체의 소식) */}
-                {/* [사업종료] 단체명의 기부사업명이 사업 종료되었습니다. 설문조사를 해주세요.*/}
-                {/* [첨부파일업로드] 단체명의 어떤 기부사업이 업데이트 되었습니다. 확인해주세요!*/}
-                {/* [관심단체] 관심단체명의 새로운 소식이 업데이트 되었습니다.*/}
-                {/*alarmType == 0 인 경우*/}
-                <div>[사업종료] {news.orgName} 의 "{news.bizName}" 사업이 사업 종료되었습니다.</div> 
-                {/*alarmType == 1 인 경우*/}
-                <div>[첨부파일업로드] {news.orgName} 의 "{news.bizName}"" 사업이 업데이트 되었습니다. </div>
-                {/*alarmType == 2 인 경우*/}
-                <div>[관심단체] {news.orgName} 의 새로운 소식이 업데이트 되었습니다. </div>
-
-                <div><span>{news.bizName}</span> | <span>{news.alarmDate}</span></div>
+        <div className="news-info">
+            <div>{content}</div>
+            <div>
+                <span>{news.bizName}</span> | <span>{news.alarmDate}</span>
             </div>
-    )
+        </div>
+    );
 }
+
 
 
 
