30850744a88b6161419e22d17c89b8c09a7fd18f
hi09067@gmail.com <hi09067@gmail.com>
2025-07-07 09:35:36 +0900

    [김은서] 백엔드 : 기부 내역 조회 기능 mapper 수정

--- back/don_give_up_back/src/main/java/kr/or/iei/member/model/dto/MemberDonation.java
+++ back/don_give_up_back/src/main/java/kr/or/iei/member/model/dto/MemberDonation.java
@@ -13,4 +13,10 @@ public class MemberDonation {
 	private String orgName;				//단체명
 	private String donateMoney;			//기부금액 (단위 9,999,999) 하고자 String
 	private String donateDate;			//기부날짜
+	
+	// join 을 위한 추가 변수 선언
+	private String orgBizNum;			// 사업자번호
+	private String orgAddrMain;			// 메인 주소
+	private String orgAddrDetail;		// 상세 주소
+	private int bizNo;					// 사업번호
 }
