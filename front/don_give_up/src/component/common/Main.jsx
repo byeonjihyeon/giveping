eb088a4cb66360a8301be90fdab16f98086a9087
DESKTOP-KQI7EDO\user1 <byeonchoco@gamil.com>
2025-07-15 08:49:45 +0900

    [변지현] (프론트 메인 페이지 일부내용 추가)

--- front/don_give_up/src/component/common/Main.jsx
+++ front/don_give_up/src/component/common/Main.jsx
@@ -1,7 +1,13 @@
+
+import { useState } from "react";
 import MainList from "./MainList";
 
 
 export default function Main(){
+    const [categories, setCategories] = useState([
+        '아동 👧', '노인 👨‍🦳', '난민 🌍', '환경 🌳', '장애인 🤟', '교육 🎨', '재해지원 💧'
+    ]);
+
     return (
         <>
         <div className="banner-wrap">
@@ -9,7 +15,14 @@ export default function Main(){
                 <span>관심있는 카테고리를 선택해주세요 ! </span>
             </div>
             <div className="search-bar">
+                <div className="">
 
+                </div>
+                <div className="main-category">
+                    {categories.map(function(ctg, index){
+                        return <div>{ctg}</div>
+                    })}
+                </div>
             </div>
         </div>
 
