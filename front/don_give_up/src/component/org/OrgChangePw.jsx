ee572766a061a21547173ff7654a39e7c1bfe514
DESKTOP-6LHTD8G\user1 <qor659659@gmail.com>
2025-07-16 12:43:32 +0900

    [백인환] 프론트 : 단체 비밀번호 변경 CSS 변경

--- front/don_give_up/src/component/org/OrgChangePw.jsx
+++ front/don_give_up/src/component/org/OrgChangePw.jsx
@@ -18,56 +18,80 @@ export default function OrgChangePw(){
     const orgNo = loginOrg.orgNo;
 
     const [org, setOrg] = useState({orgNo : orgNo, orgPw : ""});
+    const [isCheck, setIsCheck] = useState(false);      //비밀번호 확인 성공 여부
     const [newOrgPw, setNewOrgPw] = useState("");       //새 비밀번호
     const [newOrgPwRe, setNewOrgPwRe] = useState("");   //새 비밀번호 확인
-    const [result, setResult] = useState(false);        //비밀번호 확인을 진행해야 새 비밀번호를 입력할 수 있도록 제어할 변수
     const pwRef = useRef(null);
-    const pwReRef = useRef(null);
+    const newPwRef = useRef(null);
+    const newPwReRef = useRef(null);
 
-    //비밀번호 변경 시 호출 함수
+    //현재 비밀번호 변경 시 호출 함수
     function chgOrgPw(e){
         setOrg({...org, orgPw : e.target.value});
-    }
-    function chgNewOrgPw(e){
-        setNewOrgPw(e.target.value);
-    }
-    function chgNewOrgPwRe(e){
-        setNewOrgPwRe(e.target.value);
-    }
 
-    //현재 비밀번호 확인 버튼 클릭 시 실행 함수
-    function checkPw(){
-        if(org.orgPw == ""){
-            Swal.fire({
-                title : "알림",
-                text : "비밀번호를 입력하세요.",
-                icon : "warning",
-                confirmButtonText : "확인",
-                didClose : pwRef.current.focus()
-            });
-            return;
-        }
-
+        //현재 비밀번호가 일치하는지 확인
         let options = {};
         options.url = serverUrl + "/org/chkPw";
         options.method = "post";
-        options.data = org;
-
+        options.data = {orgNo : orgNo, orgPw : e.target.value};
+    
         axiosInstance(options)
         .then(function(res){
-            Swal.fire({
-                title : "알림",
-                text : res.data.clientMsg,
-                icon : res.data.alertIcon,
-                confirmButtonText : "확인",
-                didClose : pwRef.current.focus()
-            })
-            .then(function(result){
-                if(res.data.resData){
-                    setResult(true);
-                }
-            });
+            if(res.data.resData){
+                setIsCheck(true);
+            }else{
+                setIsCheck(false);
+            }
         });
+
+        if(newOrgPw != "" && newOrgPw == e.target.value){
+            setPwChk(5);
+        }else{
+            setPwChk(0);
+        }
+    }
+
+    //새 비밀번호 변경 시
+    function chgNewOrgPw(e){
+        setNewOrgPw(e.target.value);
+
+        //비밀번호 정규표현식
+        const regExp = /^[a-zA-Z0-9!@#$]{6,30}$/; //영어 대소문자, 특수문자, 숫자 6~30글자
+
+        if(!regExp.test(e.target.value)){
+            setPwChk(2); //유효성 체크 실패
+        }else if(newOrgPwRe != ""){ //새 비밀번호 확인 입력한 경우
+            if(e.target.value == newOrgPwRe){ //새 비밀번호 == 새 비밀번호 확인
+                if(e.target.value == org.orgPw){ //새 비밀번호 == 기존 비밀번호
+                    setPwChk(5);
+                }else{
+                    setPwChk(1);
+                }
+            }else{
+                setPwChk(3);
+            }
+        }else{ //새 비밀번호 확인 입력 안한 경우
+            setPwChk(4);
+        }
+    }
+
+    //새 비밀번호 확인 변경 시
+    function chgNewOrgPwRe(e){
+        setNewOrgPwRe(e.target.value);
+
+        const regExp = /^[a-zA-Z0-9!@#$]{6,30}$/;
+
+        if(newOrgPw == e.target.value){ //새 비밀번호 == 새 비밀번호 확인
+            if(regExp.test(newOrgPw)){
+                if(newOrgPw == org.orgPw){ //새 비밀번호 == 기존 비밀번호
+                    setPwChk(5);
+                }else{
+                    setPwChk(1);
+                }
+            }
+        }else{ //새 비밀번호와 불일치
+            setPwChk(3);
+        }
     }
 
     /*
@@ -81,49 +105,18 @@ export default function OrgChangePw(){
     */
     const [pwChk, setPwChk] = useState(0);
 
-    function checkNewPw(e){
-        //비밀번호 정규표현식
-        const regExp = /^[a-zA-Z0-9!@#$]{6,30}$/; //영어 대소문자, 특수문자, 숫자 6~30글자
 
-        if(e.target.id == "newOrgPw"){ //새 비밀번호 입력
-            if(!regExp.test(e.target.value)){
-                setPwChk(2); //유효성 체크 실패
-            }else if(newOrgPwRe != ""){ //새 비밀번호 확인 입력한 경우
-                if(e.target.value == newOrgPwRe){ //새 비밀번호 == 새 비밀번호 확인
-                    if(e.target.value == org.orgPw){ //새 비밀번호 == 기존 비밀번호
-                        setPwChk(5);
-                    }else{
-                        setPwChk(1);
-                    }
-                }else{
-                    setPwChk(3);
-                }
-            }else{ //새 비밀번호 확인 입력 안한 경우
-                setPwChk(4);
-            }
-        }else{ //새 비밀번호 확인 입력
-            if(newOrgPw == e.target.value){ //새 비밀번호 == 새 비밀번호 확인
-                if(regExp.test(newOrgPw)){
-                    if(newOrgPw == org.orgPw){ //새 비밀번호 == 기존 비밀번호
-                        setPwChk(5);
-                    }else{
-                        setPwChk(1);
-                    }
-                }
-            }else{ //새 비밀번호와 불일치
-                setPwChk(3);
-            }
-        }
-    }
 
     //변경 버튼 클릭 시 실행할 함수
     function updatePw(){
         const validations = [
-            {valid : newOrgPw == "", message : "변경할 비밀번호를 입력하세요.", inputRef : pwRef},
-            {valid : pwChk == 2, message : "변경할 비밀번호 형식이 올바르지 않습니다.", inputRef : pwRef},
-            {valid : pwChk == 5, message : "변경할 비밀번호가 기존 비밀번호와 같습니다.", inputRef : pwRef},
-            {valid : newOrgPwRe == "", message : "비밀번호 확인을 입력하세요.", inputRef : pwReRef},
-            {valid : pwChk == 3, message : "변경할 비밀번호와 비밀번호 확인이 일치하지 않습니다.", inputRef : pwReRef}
+            {valid : org.orgPw == "", message : "현재 비밀번호를 입력하세요.", inputRef : pwRef},
+            {valid : !isCheck, message : "현재 비밀번호가 일치하지 않습니다.", inputRef : pwRef},
+            {valid : pwChk == 2 , message : "새 비밀번호 형식이 올바르지 않습니다.", inputRef : newPwRef},
+            {valid : pwChk == 5 , message : "새 비밀번호가 현재 비밀번호와 같습니다.", inputRef : newPwRef},
+            {valid : newOrgPw == "", message : "새 비밀번호를 입력하세요.", inputRef : newPwRef},
+            {valid : newOrgPwRe == "", message : "새 비밀번호 확인을 입력하세요.", inputRef : newPwReRef},
+            {valid : pwChk == 0, message : "새 비밀번호를 다시 입력해주세요.", inputRef : newPwRef}
         ];
 
         for(let i=0; i<validations.length; i++){
@@ -139,6 +132,10 @@ export default function OrgChangePw(){
             }
         }
 
+        if(pwChk != 1){
+            return;
+        }
+
         let options = {};
         options.url = serverUrl + "/org/updPw/" + orgNo + "/" + newOrgPw;
         options.method = "post";
@@ -152,6 +149,7 @@ export default function OrgChangePw(){
                 confirmButtonText : "확인"
             })
             .then(function(result){
+                setIsCheck(false);
                 setLoginOrg(null);
                 setIsLogined(false);
                 navigate("/login");
@@ -160,6 +158,41 @@ export default function OrgChangePw(){
     }
 
     return (
+        <div className="pwchange-wrap">
+            <div className="pwchange-title">
+                <span>비밀번호 변경</span>
+            </div>
+            <div className="pwchange-content"> 
+                <span>- 이전에 사용한적 없는 비밀번호가 안전합니다.</span>
+                <span>- 다른 아이디/사이트에서 사용한 적 없는 비밀번호</span>
+                <span>- 영어 대소문자, 특수문자, 숫자 6~30글자</span>
+            </div>
+            <form style={{width : "100%"}} autoComplete="off" onSubmit={function(e){
+                e.preventDefault();
+                updatePw();
+            }}>
+                <div className="pw-input-wrap">        
+                    <input type="password" id="orgPw" placeholder="현재 비밀번호" value={org.orgPw} onChange={chgOrgPw} ref={pwRef}/>
+                </div>
+                <div className="pwRe-wrap">
+                    <div className="pw-input-wrap">
+                        <input type="password" id="newOrgPw" placeholder="새 비밀번호" value={newOrgPw} onChange={chgNewOrgPw} ref={newPwRef}/>
+                    </div>
+                    <div className="pw-input-wrap">
+                        <input type="password" id="newOrgPwRe" placeholder="새 비밀번호 확인"  value={newOrgPwRe} onChange={chgNewOrgPwRe} ref={newPwReRef}/>
+                    </div>
+                    <div className="pwRe-msg">
+                        <p style={{color : "red"}}>{pwChk == 2 ? "*새 비밀번호 형식이 올바르지 않습니다."
+                            : pwChk == 5 ? "*새 비밀번호가 기존 비밀번호와 같습니다."
+                            : pwChk == 3 ? "*새 비밀번호와 비밀번호 확인이 일치하지 않습니다." : ""}</p>
+                    </div>      
+                </div>
+                <div className="update-pw-btn">
+                    <button type="submit">변경</button>
+                </div>
+            </form>
+        </div>  
+        /*
         <div>
             <h2 className="page-title" style={{textAlign : "left", marginLeft : "20px"}}>비밀번호 변경</h2>
             <div className={!result ? "updPw-div" : "updPw-div-plus"}>
@@ -210,6 +243,6 @@ export default function OrgChangePw(){
                 }
             </div>
         </div>
-        
+        */
     )
 }
